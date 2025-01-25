bff6f5b11f0d197112f55815acfb7b28053d6b37
skomarov <derpartizanen@yandex.ru>
2025-01-25 14:07:11 +0300

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/derpartizanen/metrics/internal/config"
 	"github.com/derpartizanen/metrics/internal/handler"
+	"github.com/derpartizanen/metrics/internal/logger"
 	"github.com/derpartizanen/metrics/internal/repository/memstorage"
 	"github.com/derpartizanen/metrics/internal/storage"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func main() {
 	cfg := config.ConfigureServer()
+	err := logger.Initialize("INFO")
 	repository := memstorage.New()
 	store := storage.New(repository)
 	h := handler.NewHandler(store)
 
 	r := chi.NewRouter()
+	r.Use(logger.RequestLogger)
 	r.Get("/", h.GetAllHandler)
 	r.Get("/value/{metricType}/{metricName}", h.GetHandler)
 	r.Post("/update/{metricType}/{metricName}/{metricValue}", h.UpdateHandler)
 
-	log.Println("Starting server on", cfg.Host)
-	err := http.ListenAndServe(cfg.Host, r)
+	logger.Log.Info("Starting server on", zap.String("host", cfg.Host))
+	err = http.ListenAndServe(cfg.Host, r)
 	if err != nil {
 		panic(err)
 	}
