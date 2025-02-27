91bc9e4f8ead0b6a07856700c406cc56441355b6
skomarov <derpartizanen@yandex.ru>
2025-02-27 18:14:14 +0300

    iter15

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,173 +1,66 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
-	"errors"
-	"fmt"
+	"context"
 	"log"
-	"math/rand"
 	"net/http"
-	"reflect"
-	"runtime"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/derpartizanen/metrics/internal/agent"
 	"github.com/derpartizanen/metrics/internal/config"
-	"github.com/derpartizanen/metrics/internal/hash"
 	"github.com/derpartizanen/metrics/internal/logger"
 	"github.com/derpartizanen/metrics/internal/model"
-	"go.uber.org/zap"
 )
-
-var (
-	ErrDoRequest = errors.New("execution request error")
-)
-
-var cfg *config.AgentConfig
-var counter int64
 
 func main() {
-	var metrics []model.Metrics
-
 	err := logger.Initialize("INFO")
 	if err != nil {
 		log.Fatal(err)
 	}
 	logger.Log.Info("Starting agent")
 
-	cfg = config.ConfigureAgent()
+	cfg := config.ConfigureAgent()
 	cfg.LogVars()
 
-	go func() {
-		for {
-			metrics = updateMetrics()
-			time.Sleep(time.Duration(cfg.PollInterval) * time.Second)
-		}
-	}()
+	client := &http.Client{
+		Timeout: time.Minute,
+	}
+
+	metricAgent := agent.New(client, cfg)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGKILL, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	run(ctx, metricAgent)
+}
+
+func run(ctx context.Context, agent *agent.Agent) {
+
+	pollTicker := time.NewTicker(time.Duration(agent.Config.PollInterval) * time.Second)
+	reportTicker := time.NewTicker(time.Duration(agent.Config.ReportInterval) * time.Second)
+
+	defer pollTicker.Stop()
+	defer reportTicker.Stop()
+
+	jobs := make(chan []model.Metrics, agent.Config.RateLimit)
+	defer close(jobs)
+
+	for i := 0; i < agent.Config.RateLimit; i++ {
+		go agent.Worker(ctx, i+1, jobs)
+	}
 
 	for {
-		time.Sleep(time.Duration(cfg.ReportInterval) * time.Second)
-		reportMetricsWithRetry(metrics, 3)
-	}
-}
-
-func updateMetrics() []model.Metrics {
-	var metrics []model.Metrics
-	var memStats runtime.MemStats
-
-	runtime.ReadMemStats(&memStats)
-	mValue := reflect.ValueOf(memStats)
-	mType := mValue.Type()
-
-	for _, metricName := range model.GaugeMetrics {
-		field, ok := mType.FieldByName(metricName)
-		if !ok {
-			continue
+		select {
+		case <-pollTicker.C:
+			go agent.CollectMemStatsMetrics()
+			go agent.CollectPsutilMetrics()
+		case <-reportTicker.C:
+			go agent.AddReportJob(jobs)
+		case <-ctx.Done():
+			logger.Log.Info("shutting down agent...")
+			return
 		}
-
-		var value float64
-
-		switch mValue.FieldByName(metricName).Interface().(type) {
-		case uint64:
-			value = float64(mValue.FieldByName(metricName).Interface().(uint64))
-		case uint32:
-			value = float64(mValue.FieldByName(metricName).Interface().(uint32))
-		case float64:
-			value = mValue.FieldByName(metricName).Interface().(float64)
-		default:
-			return nil
-		}
-
-		metrics = append(metrics, model.Metrics{ID: field.Name, MType: "gauge", Value: &value})
 	}
-
-	counter += 1
-	metrics = append(metrics, model.Metrics{ID: "PollCount", MType: "counter", Delta: &counter})
-	random := rand.Float64()
-	metrics = append(metrics, model.Metrics{ID: "RandomValue", MType: "gauge", Value: &random})
-
-	return metrics
-}
-
-func reportMetricsWithRetry(metrics []model.Metrics, retryCount int) {
-	var err error
-	for i := 1; i <= retryCount; i++ {
-		err = reportMetrics(metrics)
-		if i == retryCount {
-			break
-		}
-		if err != nil && errors.Is(err, ErrDoRequest) {
-			logger.Log.Info(fmt.Sprintf("retry %d to report metrics", i))
-			time.Sleep(time.Duration(i+i-1) * time.Second)
-			continue
-		}
-		break
-	}
-
-	if err != nil {
-		logger.Log.Error("failed to report metrics", zap.Error(err))
-	}
-}
-
-func reportMetrics(metrics []model.Metrics) error {
-	if len(metrics) == 0 {
-		return errors.New("empty metrics")
-	}
-
-	reportURL := fmt.Sprintf("http://%s/updates/", cfg.ReportEndpoint)
-	client := &http.Client{}
-
-	jsonStr, err := json.Marshal(metrics)
-	if err != nil {
-		return errors.New("can't marshal data")
-	}
-
-	gzipData, err := compressData(jsonStr)
-	if err != nil {
-		return errors.New("can't compress data")
-	}
-
-	req, err := http.NewRequest(http.MethodPost, reportURL, bytes.NewBuffer(gzipData))
-	if err != nil {
-		return errors.New("new request error")
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Content-Encoding", "gzip")
-
-	if cfg.HashKey != "" {
-		hash := hash.Calc(cfg.HashKey, jsonStr)
-		req.Header.Add("HashSHA256", hash)
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return ErrDoRequest
-	}
-
-	logger.Log.Debug(fmt.Sprintf("send batch request with %d metrics", len(metrics)))
-	res.Body.Close()
-
-	return nil
-}
-
-func compressData(data []byte) ([]byte, error) {
-	var b bytes.Buffer
-	w, err := gzip.NewWriterLevel(&b, gzip.BestSpeed)
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = w.Write(data)
-	if err != nil {
-		return nil, err
-	}
-
-	err = w.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return b.Bytes(), nil
 }
---
+++ internal/agent/agent.go
@@ -0,0 +1,183 @@
+package agent
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"reflect"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/derpartizanen/metrics/internal/compressor"
+	"github.com/derpartizanen/metrics/internal/config"
+	"github.com/derpartizanen/metrics/internal/hash"
+	"github.com/derpartizanen/metrics/internal/logger"
+	"github.com/derpartizanen/metrics/internal/model"
+	"github.com/shirou/gopsutil/v3/mem"
+	"go.uber.org/zap"
+)
+
+type Agent struct {
+	Config  *config.AgentConfig
+	Client  *http.Client
+	Metrics []model.Metrics
+	mu      sync.Mutex
+}
+
+var (
+	counter      int64
+	ErrDoRequest = errors.New("execution request error")
+)
+
+func New(client *http.Client, config *config.AgentConfig) *Agent {
+	return &Agent{Client: client, Metrics: []model.Metrics{}, Config: config}
+}
+
+func (agent *Agent) CollectPsutilMetrics() {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		logger.Log.Error("virtual memory error", zap.Error(err))
+		return
+	}
+
+	totalMemory := float64(vm.Total)
+	freeMemory := float64(vm.Free)
+	CPUutilization := vm.UsedPercent
+
+	agent.SetGaugeMetric("TotalMemory", &totalMemory)
+	agent.SetGaugeMetric("FreeMemory", &freeMemory)
+	agent.SetGaugeMetric("CPUutilization1", &CPUutilization)
+}
+
+func (agent *Agent) CollectMemStatsMetrics() {
+	var memStats runtime.MemStats
+
+	runtime.ReadMemStats(&memStats)
+	mValue := reflect.ValueOf(memStats)
+	mType := mValue.Type()
+
+	for _, metricName := range model.GaugeMetrics {
+		field, ok := mType.FieldByName(metricName)
+		if !ok {
+			continue
+		}
+
+		var value float64
+
+		switch mValue.FieldByName(metricName).Interface().(type) {
+		case uint64:
+			value = float64(mValue.FieldByName(metricName).Interface().(uint64))
+		case uint32:
+			value = float64(mValue.FieldByName(metricName).Interface().(uint32))
+		case float64:
+			value = mValue.FieldByName(metricName).Interface().(float64)
+		}
+
+		agent.SetGaugeMetric(field.Name, &value)
+	}
+
+	counter += 1
+	agent.SetCounterMetric("PollCount", &counter)
+
+	random := rand.Float64()
+	agent.SetGaugeMetric("RandomValue", &random)
+}
+
+func (agent *Agent) SetGaugeMetric(metricName string, metricValue *float64) {
+	agent.mu.Lock()
+	defer agent.mu.Unlock()
+
+	agent.Metrics = append(agent.Metrics, model.Metrics{ID: metricName, MType: model.MetricTypeGauge, Value: metricValue})
+}
+
+func (agent *Agent) SetCounterMetric(metricName string, metricDelta *int64) {
+	agent.mu.Lock()
+	defer agent.mu.Unlock()
+
+	agent.Metrics = append(agent.Metrics, model.Metrics{ID: metricName, MType: model.MetricTypeCounter, Delta: metricDelta})
+}
+
+func (agent *Agent) AddReportJob(jobs chan<- []model.Metrics) {
+	jobs <- agent.Metrics
+}
+
+func (agent *Agent) Worker(ctx context.Context, id int, jobs <-chan []model.Metrics) {
+	for metrics := range jobs {
+		logger.Log.Debug(fmt.Sprintf("start job on worker %d", id))
+
+		err := agent.reportMetricsWithRetry(ctx, metrics)
+		if err != nil {
+			logger.Log.Error("send request error", zap.Error(err))
+		}
+
+		logger.Log.Debug(fmt.Sprintf("finish job on worker %d", id))
+	}
+}
+
+func (agent *Agent) reportMetricsWithRetry(ctx context.Context, metrics []model.Metrics) error {
+	var err error
+	for i := 1; i <= agent.Config.ReportRetryCount; i++ {
+		err = agent.reportMetrics(ctx, metrics)
+		if i == agent.Config.ReportRetryCount {
+			break
+		}
+		if err != nil && errors.Is(err, ErrDoRequest) {
+			logger.Log.Info(fmt.Sprintf("retry %d to report metrics", i))
+			time.Sleep(time.Duration(i+i-1) * time.Second)
+			continue
+		}
+		break
+	}
+
+	if err != nil {
+		logger.Log.Error("failed to report metrics", zap.Error(err))
+	}
+
+	return err
+}
+
+func (agent *Agent) reportMetrics(ctx context.Context, metrics []model.Metrics) error {
+	if len(metrics) == 0 {
+		return nil
+	}
+
+	reportURL := fmt.Sprintf("http://%s/updates/", agent.Config.ReportEndpoint)
+
+	jsonStr, err := json.Marshal(metrics)
+	if err != nil {
+		return errors.New("can't marshal data")
+	}
+
+	gzipData, err := compressor.Compress(jsonStr)
+	if err != nil {
+		return errors.New("can't compress data")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reportURL, bytes.NewBuffer(gzipData))
+	if err != nil {
+		return errors.New("new request error")
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Encoding", "gzip")
+
+	if agent.Config.HashKey != "" {
+		requestHash := hash.Calc(agent.Config.HashKey, jsonStr)
+		req.Header.Add("HashSHA256", requestHash)
+	}
+
+	res, err := agent.Client.Do(req)
+	if err != nil {
+		return ErrDoRequest
+	}
+
+	logger.Log.Debug(fmt.Sprintf("send batch request with %d metrics", len(metrics)))
+	res.Body.Close()
+
+	return nil
+}
